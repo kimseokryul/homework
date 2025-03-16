ac510dd5561ed9b26458abfd62c79af8fcd46d6c
kimseokryul <kimseokryul96@gmail.com>
2025-03-17 08:48:17 +0900

    Add files via upload

---
+++ 확인문제/src/자판기/Beverage.java
@@ -0,0 +1,14 @@
+package 자판기;
+
+public class Beverage {
+
+	String name;
+	int price;
+	int stock;
+	
+	public Beverage(String name, int price, int stock) {
+	        this.name = name;
+	        this.price = price;
+	        this.stock = stock;
+	    }
+}
